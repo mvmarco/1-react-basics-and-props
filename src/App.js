559aaf664535eb749f8f6ce19eed8c266752c37d
marco corapi <marco.corapi@outlook.it>
2021-03-07 12:43:17 +0100

    basics

--- src/App.js
+++ src/App.js
@@ -1,6 +1,76 @@
+//##################################################
+// WHAT DOES REACT CODE LOOK LIKE BEHIND THE SCENE
+//##################################################
+/* 
+  in order to let React work you need:
+  1. React library (the general package)
+  2. React Dom (this is for dom manipulation and to use React for the browser
+     so in that case you'd need another script, as well as for App development and VR and many
+     other). So in sum, they separeted this library to render/put on the screen in the browser.
+  ------------------------------------------------------------------------------------------------
+  This is the first step, the createElement takes 3 parameters:
+  1. The HTML element such as, h1
+  2. The property such as ID or Classes or Null
+  3. The content in between the tags, like the text in a button
+  This is coming from the React library from the general package.
+
+  React.createElement('h1', null, 'hello react')
+
+  The we need to render/put our React code in the browser with React Dom.
+  This is the Second step and takes 2 parameters:
+  1. The element or the app that you want to render on the screen (step one)
+  2. The location where you want to render that app out, like the HTML location where
+     you wanna put the code. Usually it is a Div with an id of root.
+
+  ReactDOM.render(React.createElement('h1', null, 'hello react'), document.querySelector('#root))
+  ------------------------------------------------------------------------------------------------
+  Usually for scalability reasons you have a function, called functionApp(),
+  that return our element(step one) 
+  
+  function App() {
+    return (React.createElement('h1', null, 'hello react'))
+  }
+
+  the step 2 will be to pass that function in the ReactDOM.render
+
+  ReactDOM.render(React.createElement(App), document.querySelector('#root))
+
+  ------------------------------------------------------------------------------------------------
+  Now if you wanna add multiple elements you cannot just add them in the return() in the
+  function App() because it will be to heavy. So what we can do is: in the React.createElement
+  we could pass as the first parameter a 'div', like a father containeer, and add as third
+  parameter(the things between tags) an array that takes in another React.createElement or
+  multiple. As follow: 
+
+  function App() {
+    return (React.createElement('div', null, [
+      React.createElement('h1', null, 'hello react'),
+      React.createElement('button', null, 'click me!!')
+    ]))
+  }
+  ------------------------------------------------------------------------------------------------
+  to make it more interesting we could add variables and replace the text of an H1 for instance.
+  just to show that you can create elements and adding JS functionalities attached to it.
+  And you can also add CSS. Just look the H1 again. Style it is inside an object and takes
+  another one. So in sum with React, you do everything in one file. HTML generation,
+  JavaScript functions and variables, and CSS.
+
+    function App() {
+      const time = new Date().toLocaleDateString()
+      const s = {fontSize: 50, color: 'red'}
+      return (React.createElement('div', null, [
+        React.createElement('h1', {style: s}, time),
+        React.createElement('h2', {style: {color: red}}, time),
+        React.createElement('button', null, 'click me!!')
+      ]))
+  }
+*/
+
+
+
 import logo from './logo.svg';
 import './App.css';
-
+// test 
 function App() {
   return (
     <div className="App">
