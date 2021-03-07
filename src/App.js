3f801b983fa8910540bec99e52059e3e11206333
marco corapi <marco.corapi@outlook.it>
2021-03-07 13:22:37 +0100

    components

--- src/App.js
+++ src/App.js
@@ -92,7 +92,20 @@
   }
 */
 
+//##################################################
+// COMPONENTS
+//##################################################
 
+/* 
+  Conceptually, components are like JavaScript functions. They accept arbitrary inputs 
+  (called “props”) and return React elements describing what should appear on the screen.
+  The simplest way to define a component is to write a JavaScript function:
+
+  function Welcome(props) {
+    return <h1>Hello, {props.name}</h1>;
+  }
+
+*/
 
 
 import logo from './logo.svg';
